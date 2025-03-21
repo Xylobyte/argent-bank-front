79dca68e2a91487ea8a868dcfe4fc6314069d82a
Nantsa Montillet <nantsamontillet@xylobyte.com>
2025-03-21 14:40:44 +0100

    Add base pages, and resources

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
+import {BrowserRouter, Route, Routes} from "react-router";
+import Home from "./pages/Home.tsx";
+import HeaderNav from "./components/HeaderNav.tsx";
+import Footer from "./components/Footer.tsx";
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<div></div>
+		<BrowserRouter>
+			<HeaderNav/>
+
+			<Routes>
+				<Route path="/" element={<Home/>}/>
+			</Routes>
+
+			<Footer/>
+		</BrowserRouter>
 	</StrictMode>
 );
