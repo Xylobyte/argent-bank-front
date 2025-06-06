e93e4fc36e044f0716bf381c2d503e3526d3d81a
Nantsa Montillet <nantsamontillet@xylobyte.com>
2025-06-06 22:25:51 +0200

    Translate to english

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -63,7 +63,7 @@ function Profile() {
 					)}
 				</h1>
 				<button className='edit-button' onClick={toggleEditName}>
-					{isEditName ? 'Enregistrer' : 'Edit Name'}
+					{isEditName ? 'Save' : 'Edit Name'}
 				</button>
 			</div>
 			<h2 className='sr-only'>Accounts</h2>
