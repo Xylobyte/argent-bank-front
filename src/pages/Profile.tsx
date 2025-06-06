9e27d2f715277037f7935474eee35fdee2fdb9b1
Nantsa Montillet <nantsamontillet@xylobyte.com>
2025-06-06 22:20:00 +0200

    Change number

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -88,7 +88,7 @@ function Profile() {
 					<h3 className='account-title'>
 						Argent Bank Savings (x6712)
 					</h3>
-					<p className='account-amount'>$10,928,000,000.42</p>
+					<p className='account-amount'>$10,928.42</p>
 					<p className='account-amount-description'>
 						Available Balance
 					</p>
